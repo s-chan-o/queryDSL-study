576675ddc17e0592e552fa195a00283f83069433
김승찬 <s24041@gsm.hs.kr>
2025-04-05 21:51:15 +0900

    feat: add orderItem entity

---
+++ src/main/java/study/querydsl/domain/order/entity/OrderItem.java
@@ -0,0 +1,26 @@
+package study.querydsl.domain.order.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import study.querydsl.domain.menu.entity.Menu;
+
+@Entity
+@Table(name = "order_items")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Menu menu;
+
+    private int quantity;
+}
