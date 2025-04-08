7bc65a1f9530ad625c7c4fc1d6adbd1bd5036d67
김승찬 <s24041@gsm.hs.kr>
2025-04-08 10:00:30 +0900

    feat: add CustomException

---
+++ src/main/java/study/querydsl/global/exception/CustomException.java
@@ -0,0 +1,10 @@
+package study.querydsl.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CustomException extends RuntimeException {
+    private final ErrorCode errorCode;
+}
